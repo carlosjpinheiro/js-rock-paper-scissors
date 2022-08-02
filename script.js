bcaf1ef3c9c4378b52bc1f5d3046d316fe9399a6
Carlos J Pinheiro <carlos.pnx@gmail.com>
2022-08-02 16:14:56 -0300

    Refactor JS code to support DOM methods and make HTML and CSS

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let humanScore = 0;
+let robotScore = 0; 
+const humanScoreDiv = document.querySelector('#human');
+const robotScoreDiv = document.querySelector('#robot');
+const buttons = document.querySelectorAll('.buttons');
+const resetButton = document.querySelector('#reset');
+const finalMsg = document.querySelector('#final');
+
 function getComputerChoice () {
     let rand = Math.floor(Math.random() * 3 ) + 1;
     switch (rand) {
@@ -16,41 +24,54 @@ function getComputerChoice () {
 function playRound(human, robot) {
     if (human == robot) {
         return 0
-    } else if (human == "rock" && robot == "paper") {
+    } else if ((human == "rock" && robot == "paper") || (human == "paper" && robot == "scissors") || (human == "scissors" && robot == "rock")) {
         return 1
-    } else if (human == "rock" && robot == "scissors") {
-        return 9
-    } else if (human == "paper" && robot == "scissors") {
-        return 1
-    } else if (human == "paper" && robot == "rock") {
-        return 9
-    } else if (human == "scissors" && robot == "paper") {
-        return 9
-    } else if (human == "scissors" && robot == "rock") {
-        return 1   
+    } else if ((human == "rock" && robot == "scissors") || (human == "paper" && robot == "rock") || (human == "scissors" && robot == "paper")) {
+        return 9 
     }
 }
 
-function game() {
-    let humanScore = 0;
-    let robotScore = 0;        
-    while (humanScore < 5 && robotScore < 5) {
-        let robotSelection = getComputerChoice();
-        let playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
-        while (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
-            playerSelection = prompt('Please type: Rock, Paper or Scissors').toLowerCase();
-        }
-        let jogoAtual = playRound(playerSelection, robotSelection);
-        if (jogoAtual === 0) {
-            console.log(`It's a tie`);
-        } else if (jogoAtual === 1) {
-            robotScore++;
-            console.log(`You Lose! ${robotSelection} beats ${playerSelection}`);
-        } else {
-            humanScore++;
-            console.log(`You Win! ${playerSelection} beats ${robotSelection}`);
-        }
-    }
-    console.log(`-=Score=- You: ${humanScore} Robot: ${robotScore}`);
+function reset() {
+    humanScore = 0;
+    robotScore = 0;
+    robotScoreDiv.textContent = 'Robot: 0';
+    humanScoreDiv.textContent = 'Human: 0';
+    finalMsg.textContent = '';
 }
-game();
+
+function playGame (button) {
+    if (humanScore == 5 || robotScore == 5) reset();
+    finalMsg.textContent = '';
+    let humanChoice = button.textContent.toLowerCase().replace('!','');
+    let robotChoice = getComputerChoice();
+    let currentGame = playRound(humanChoice, robotChoice);
+    if (currentGame === 0) {
+        finalMsg.textContent = 'Tie';
+    } else if (currentGame === 1) {
+        robotScore++;
+        robotScoreDiv.textContent = 'Robot: 0' + String(robotScore);
+        finalMsg.textContent = `${robotChoice.toUpperCase()} BEATS ${humanChoice.toUpperCase()}`;
+    } else {
+        humanScore++;
+        humanScoreDiv.textContent = 'Human: 0' + String(humanScore);
+        finalMsg.textContent = `${humanChoice.toUpperCase()} BEATS ${robotChoice.toUpperCase()}`;
+    }
+    if (humanScore == 5 || robotScore == 5) {
+        gameEnd();
+    }
+}
+
+function gameEnd () {
+    if (humanScore >= 5) {
+        finalMsg.textContent = `Human Victory!!! `;
+    } else {
+        finalMsg.textContent = 'Robot Victory =(';
+    }
+}
+
+resetButton.addEventListener('click', reset)
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        playGame(button)
+    })
+})
